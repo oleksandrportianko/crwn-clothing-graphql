4cd126076e0650e4e6d9523c99612c70b7a14088
Oleksandr <portianko123@gmail.com>
2022-10-16 15:05:09 +0300

    upgraded receive  categories method and added loaders

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -24,10 +24,21 @@ export const CategoriesProvider = ({ children }) => {
   const { loading, error, data } = useQuery(COLLECTIONS)
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  console.log(loading)
-  console.log(data)
+  useEffect(() => {
+    if (data) {
+      const { collections } = data;
+  
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection
+        acc[title.toLowerCase()] = items
+        return acc
+      }, {}) 
+      
+      setCategoriesMap(collectionsMap)
+    }
+  }, [data])
 
-  const value = { categoriesMap };
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
